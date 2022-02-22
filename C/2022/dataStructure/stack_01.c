cb087f62b049bee71be5c3e718e5209bc7c36f6d
seonyoungan <asy093@naver.com>
2022-02-22 18:19:52 +0900

    update stack_01.c

--- C/2022/dataStructure/stack_01.c
+++ C/2022/dataStructure/stack_01.c
@@ -1,54 +1,96 @@
 #include <stdio.h>
-#define SIZE 100
+#define SIZE 5
 
-int stack[SIZE]; //배열
+int stack[SIZE];
 int top = -1;
 
-int IsEmpty();
-int IsFull();
-void push();
+
+int isEmpty();
+int isFull();
+int push(int data);
 int pop();
+int peek();
+void printStack();
 
+int main(){
+  printStack(); //Error: Stack is Empty!!
+  push(1); //1추가
+  push(2); //2추가
+  push(3); //3추가
+  push(4); //4추가
+  push(5); //5추가
+  printStack(); // 1 2 3 4 5
+  pop(); //5삭제
+  pop(); //4삭제
+  pop(); //3삭제
+  printStack(); // 1 2
+  pop(); //2삭제
+  pop(); //Error: Stack is Empty!! 
+  push(6); //6추가
+  push(7); //7추가
+  push(8); //8추가
+  push(9); //9추가
+  push(10); //10추가
+  printStack(); // 6 7 8 9 10
+  push(11); //Alert: Stack is Full!! 
 
-int main(void){
-    push(3);
-    push(2);
-    push(12);
-    printf("%d\n", pop());
-    printf("%d\n", pop());
-    printf("%d\n", pop());
-    return 0;
+  return 0;
 }
 
-int IsEmpty(){
-    if(top<0)  //비어있는 상태
-        return true;
-    else
-        return false;
+// 0==거짓 1==참
+int isEmpty(){
+  if(top==-1){ //스택이 공백이면
+    printf("Error: Stack is Empty!\n"); //에러메세지
+    return 1; //참
+  }
+  else{
+    return 0; //거짓
+  }
 }
 
-int IsFull(){
-    if(top>=SIZE-1) //꽉찬 상태
-        return true;
-    else
-        return false;
+int isFull(){
+  if(top>=SIZE-1){ //스택이 꽉찼으면
+    printf("Error: Stack is Full!\n"); //에러메세지
+    return 1; //참
+  }
+  else{
+    return 0; //거짓
+  } 
 }
 
-void push(int data){
-    if(!IsFull()){ // stack != Full
-        top++; // topcnrk
-        stack[top] = data; //해당공간에 data변수 삽입
-    }
-    else{
-        printf("stack is full.");
-    }
+int push(int data){
+  if(isFull() == 0){ //꽉찬게 아니면
+    top++; //top에 1증가
+    stack[top] = data; //data변수에 stack[top] 대입
+  }
+  else{
+    printf("Alert: Stack is Full.\n"); //상태알림  
+  }  
 }
+
 int pop(){
-    if(!IsEmpty()){ //stack != Empty
-        top--;
-        stack[top] = data;
-    } 
-    else{
-        printf("stack is empty.");
+  if(isEmpty() == 0){ //꽉찬게 아니면
+    top--; //top에 1증가
+    return stack[top]; //==stack[data-1]
+  }
+  else{
+    printf("Alert: Stack is Empty.\n");; //상태알림  
+  }  
+}
+
+int peek(){ //스택의 현재 위치 반환
+  if(isEmpty() == 0){ //스택이 비어있지 않으면
+    return stack[top]; //현재 맨 위의 값 보여줌
+  }
+}
+
+void printStack(){
+  //stack의 모든 원소 출력
+  if(isEmpty()==0){
+    printf("\[");
+    for(int i=0;i<=top;i++){
+      printf("%d ", stack[i]);
     }
+    printf("\]\n");
+  }
 }
